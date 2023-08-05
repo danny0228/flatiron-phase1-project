aee72919ca3ad88d8b5aa62f53778c4f54944763
Daniel Tuiran <danny0228@hotmail.com>
2023-08-05 13:22:33 -0400

    added a function to update the likes when clicked and reflect the changes in the DOM without having to reload the page

--- main.js
+++ main.js
@@ -28,15 +28,36 @@ document.addEventListener("DOMContentLoaded", () => {
         Number: ${player.jerseyNumber}<br>
         Preffered foot: ${player.preferredFoot}<br>
         National team: ${player.nationalTeam}<br>
-        Position: ${player.position}<br>
-        ${player.likes} likes`
+        Position: ${player.position}`
+
+        const p2 = document.createElement('p')
+        p2.textContent = `${player.likes} likes`
+        p2.setAttribute('id', 'p2')
 
         const button = document.createElement('button')
         button.classList.add('like-btn')
         button.setAttribute('id', `${player.id}`)
         button.textContent = 'Like ❤️'
 
-        card.append(h2, img, p, button)
+        card.append(h2, img, p, p2, button)
         document.querySelector('#player-collection').appendChild(card)
+
+        card.querySelector('.like-btn').addEventListener('click', () => {
+            player.likes += 1
+            card.querySelector('#p2').textContent = `${player.likes} likes`
+            updateLikes(player)
+        })
+    }
+
+    function updateLikes(playerList) {
+        fetch(`http://localhost:3000/barcelonaPlayers/${playerList.id}`, {
+            method: 'PATCH',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(playerList)
+        })
+            .then(res => res.json())
+            .then(player => console.log(player))
     }
 })
