7c6e4f20d979d47e3ff1e7f8c60de06301955ce5
Daniel Tuiran <danny0228@hotmail.com>
2023-08-06 20:53:26 -0400

    added comment code to my document to make it easier to understand

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-console.log("conectado!!")
 let addPlayer = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-
+    //fetch all the info from the json
     fetch("http://localhost:3000/barcelonaPlayers")
         .then(res => res.json())
         .then(renderPlayers)
-
+    //use the renderPlayers function to call a forEach iteration for every object in the json 
     function renderPlayers(playerList) {
         playerList.forEach((player) => {
             renderCard(player)
         })
     }
-
+    //use the renderCard function to create a card and their respective html elements for each object in the json
     function renderCard(player) {
         const card = document.createElement('div')
         card.classList.add('card')
@@ -48,6 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
             card.querySelector('#p2').textContent = `${player.likes} likes`
             updateLikes(player)
         })
+        //put all the images in a variable and then iterating over all of them to add an event listener that will change their size and bring it back to normal
         let images = document.querySelectorAll('img')
         images.forEach(image => {
             image.addEventListener('mouseover', () => {
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
             })
         })
     }
-
+    // use the updateLikes function to call a fetch with a PATCH method to change the number of likes in the DOM and the json without having to reload the application, and to make sure even when the application is reloaded the changes will stay
     function updateLikes(playerList) {
         fetch(`http://localhost:3000/barcelonaPlayers/${playerList.id}`, {
             method: 'PATCH',
@@ -74,12 +74,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const addBtn = document.querySelector("#new-player-btn")
     const formContainer = document.querySelector(".container")
     const playerForm = document.querySelector(".add-favorite-player")
-
+    //adding an event listener to the form to create a new card based on the information submitted by the user
     playerForm.addEventListener('submit', (e) => {
         e.preventDefault()
         const data = Object.fromEntries(new FormData(e.target))
         data.likes = 0
-
+        //use a fetch with a POST method in order to have the new card in the DOM and added in the json 
         fetch("http://localhost:3000/barcelonaPlayers", {
             method: 'POST',
             headers: {
@@ -91,7 +91,7 @@ document.addEventListener("DOMContentLoaded", () => {
             .then(res => res.json())
             .then(data => renderCard(data))
     })
-
+    //add an event listener to a button, to have the option to hide or show the form as needed
     addBtn.addEventListener("click", () => {
         addPlayer = !addPlayer
         if (addPlayer) {
